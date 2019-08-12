c92916161c965ae59aa050945c451bb78e1b41ff
mohammadanang <m.anangnur@gmail.com>
2019-08-12 16:17:17 +0700

    add book model & new create endpoint

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ require("./db") // database connection
  * Import routes files
  */
 const index_routes = require("./routes/index")
+const book_routes = require("./routes/books")
 
 // for parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
@@ -18,6 +19,7 @@ app.get('/', (req, res) => {
  * Set routes imported
  */
 app.use("/index", index_routes)
+app.use("/book", book_routes)
 
 app.listen(3300, () => {
     console.log(`Example app listening on port 3300`)
