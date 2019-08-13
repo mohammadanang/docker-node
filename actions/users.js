161afe4c6088de9068f9c4bca4af202e76d52a27
mohammadanang <m.anangnur@gmail.com>
2019-08-13 14:58:38 +0700

    add new user endpoints

---
+++ actions/users.js
@@ -0,0 +1,43 @@
+const User = require("../models/user")
+
+const create = async (req) => {
+    let { name, email, phone } = req.body
+    phone = parseInt(phone)
+    var insert_data = {
+        name,
+        email,
+        phone
+    }
+
+    let data = new User(insert_data)
+
+    try {
+        await data.save()
+
+        return data
+    } catch(err) {
+        throw err
+    }
+}
+
+const getAll = async () => {
+    try {
+        let query = await User.find({}).exec()
+        let data = query.map((v, i) => {
+            return {
+                name: v.name,
+                email: v.email,
+                phone: v.phone
+            }
+        })
+
+        return data
+    } catch(err) {
+        throw err
+    }
+}
+
+module.exports = {
+    create,
+    getAll
+}
---
+++ routes/users.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const router = express.Router()
+const { create, getAll } = require("../actions/users")
+
+router.post("/", async (req, res) => {
+    try {
+        let data = await create(req)
+
+        return res.status(200).json({
+            status: "success",
+            data,
+            message: "User created successfully!"
+        })
+    } catch(err) {
+        return res.status(400).json({
+            status: "error",
+            message: err.message
+        })
+    }
+})
+
+router.get("/", async (req, res) => {
+    try {
+        let data = await getAll()
+
+        return res.send({
+            status: "success",
+            data,
+            message: "Get all user data"
+        })
+    } catch(err) {
+        return res.status(400).json({
+            status: "error",
+            message: err.message
+        })
+    }
+})
+
+module.exports = router
