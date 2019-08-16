6a06c8c527e1a584a1d3cd5b5fad2ed82c14a943
mohammadanang <m.anangnur@gmail.com>
2019-08-16 10:25:29 +0700

    add register user

--- routes/main.js
+++ routes/main.js
@@ -2,9 +2,11 @@ const users = require("./users")
 const index = require("./index")
 const books = require("./books")
 const shops = require("./shop.route")
+const auth = require("./auth.route")
 
 const routes = (app) => {
     app.use("/", index)
+    app.use("/auth", auth)
     app.use("/book", books)
     app.use("/shop", shops)
     app.use("/user", users)
