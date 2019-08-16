bf5fad97ff8934b5db50f3d1026eaa79103b1d67
mohammadanang <m.anangnur@gmail.com>
2019-08-16 16:29:45 +0700

    add login function

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Register = require("../actions/users/register.action")
+const Login = require("../actions/users/login.action")
 
 router.post("/register", async (req, res) => {
     try {
@@ -9,7 +10,24 @@ router.post("/register", async (req, res) => {
         return res.status(201).json({
             status: "success",
             data,
-            message: "Regiseter successfully!"
+            message: "Register successfully!"
+        })
+    } catch(err) {
+        return res.status(400).json({
+            status: "error",
+            message: err.message
+        })
+    }
+})
+
+router.post("/login", async (req, res) => {
+    try {
+        let data = await new Login(req).exec()
+
+        return res.status(200).json({
+            status: "success",
+            data,
+            message: "Login successfully!"
         })
     } catch(err) {
         return res.status(400).json({
