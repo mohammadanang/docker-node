d4a7ce06e3326a3e1a92881ba18f954dba2f0acb
mohammadanang <m.anangnur@gmail.com>
2019-10-22 17:30:40 +0700

    add realtime chat using redis & socket.io

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express')
 const app = express()
+const http = require("http").Server(app)
 require("./db") // database connection
+const io    = require("socket.io")(http),
+    redis   = require("redis"),
+    client  = redis.createClient()
 
 // for parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname))
 
 require('./routes/main')(app) // all routes imported
 
-app.listen(process.env.PORT, () => {
+app.get("/chat", (req, res, next) => {
+    res.sendFile(__dirname + '/views/template.html')
+})
+
+http.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`)
+})
+
+io.on('connection', (socket) => {
+    const subscribe = redis.createClient()
+    subscribe.subscribe('pubsub')
+
+    subscribe.on("message", (channel, message) => {
+        socket.send(message)
+    })
+
+    socket.on("message", (msg) => {
+        client.publish('pubsub', msg)
+    })
+
+    socket.on("disconnect", () => {
+        subscribe.quit()
+    })
 })
